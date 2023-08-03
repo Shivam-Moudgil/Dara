a0cb41cc1501948a8806bb176c3b5c5ba9ebb5ce
Shivam-Moudgil <shivammoudgil358@gmail.com>
2023-08-03 10:24:01 +0700

    io

---
+++ Social Media Apps/yt_Clone/src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "ec341cde4fmsh14e49a4daa94eddp106ce1jsn6b1b34f96664",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
