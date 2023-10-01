7edbe175e236edfdad0829a17ccf7ea61326c05f
Shivam-Moudgil <shivammoudgil358@gmail.com>
2023-10-02 04:03:33 +0530

    h

--- Rapid_fitness/src/components/Footer.js
+++ Rapid_fitness/src/components/Footer.js
@@ -3,11 +3,11 @@ import { Box, Stack, Typography } from '@mui/material';
 import Logo from '../assets/images/Logo.png';
 
 const Footer = () => (
-  <Box mt="80px" bgcolor="#FFF3F4">
+  <Box mt="80px" bgcolor="white">
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: '400px', height: '400px' }} />
+      <img src={Logo} alt="logo" style={{ width: '150px', height: '150px' }} />
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by JavaScript Mastery</Typography>
+    <Typography variant="h5" sx={{ fontSize: { lg: '18px', xs: '15px' } }} mt="11px" textAlign="center" pb="14px" fontWeight="bolder">Made with ❤️ by Shivam</Typography>
   </Box>
 );
 
--- Rapid_fitness/src/components/HeroBanner.js
+++ Rapid_fitness/src/components/HeroBanner.js
@@ -1,22 +1,21 @@
-import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
-import ScrollCarousel from "scroll-carousel-react";
-import HeroBannerImage from "../assets/images/banner.png";
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import ScrollCarousel from 'scroll-carousel-react';
 
 const HeroBanner = () => (
   <Box
-    sx={{ mt: { lg: "22px", xs: "20px" }, ml: { sm: "50px" } }}
+    sx={{ mt: { lg: '22px', xs: '20px' }, ml: { sm: '50px' } }}
     position="relative"
     p="20px"
   >
-    <Box display={"flex"} flexDirection={{ xs: "column", lg: "row" }}>
-      <Box width={{ xs: "100%", lg: "40%" }}>
+    <Box display="flex" flexDirection={{ xs: 'column', lg: 'row' }}>
+      <Box width={{ xs: '100%', lg: '40%' }}>
         <Typography color="#FF2625" fontWeight="600" fontSize="26px">
           Fitness Club
         </Typography>
         <Typography
           fontWeight={700}
-          sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+          sx={{ fontSize: { lg: '44px', xs: '40px' } }}
           mb="23px"
           mt="30px"
         >
@@ -30,16 +29,16 @@ const HeroBanner = () => (
           <a
             href="#exercises"
             style={{
-              marginTop: "45px",
-              textDecoration: "none",
-              width: "200px",
-              textAlign: "center",
-              background: "#FF2625",
-              padding: "14px",
-              fontSize: "22px",
-              textTransform: "none",
-              color: "white",
-              borderRadius: "4px",
+              marginTop: '45px',
+              textDecoration: 'none',
+              width: '200px',
+              textAlign: 'center',
+              background: '#FF2625',
+              padding: '14px',
+              fontSize: '22px',
+              textTransform: 'none',
+              color: 'white',
+              borderRadius: '4px',
             }}
           >
             Explore Exercises
@@ -47,32 +46,32 @@ const HeroBanner = () => (
         </Stack>
       </Box>
 
-      <Box width={{ xs: "100%", lg: "60%" }}>
+      <Box width={{ xs: '100%', lg: '60%' }}>
         <ScrollCarousel
           autoplay
           autoplaySpeed={20}
           speed={7}
-          onReady={() => console.log("I am ready")}
+          onReady={() => console.log('I am ready')}
         >
-          {["wall2.jpg", "wall3.jpg", "wall4.jpg"].map((item, index) => (
-            <Box key={index} borderRadius={"1rem"} padding={"4px"}>
-              <div style={{ position: "relative" }}>
+          {['wall2.jpg', 'wall3.jpg', 'wall4.jpg'].map((item, index) => (
+            <Box key={index} borderRadius="1rem" padding="4px">
+              <div style={{ position: 'relative' }}>
                 <img
-                  width={"600px"}
-                  height={"400px"}
-                  style={{ borderRadius: "20px", filter: "blur(1px)" }}
+                  width="600px"
+                  height="400px"
+                  style={{ borderRadius: '20px', filter: 'blur(1px)' }}
                   src={item}
                   alt="hero-banner"
                 />
                 <div
                   style={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    color: "white",
-                    fontSize: "37px",
-                    fontWeight: "bolder",
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    color: 'white',
+                    fontSize: '37px',
+                    fontWeight: 'bolder',
                   }}
                 >
                   Shivam Fitness
